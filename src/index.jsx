231a7faf600e4e60f2243bee2f77660dbc632695
OpKris <kristofferopdahl@mail.com>
2020-02-04 09:34:14 +0100

    my first commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import {Match} from './match';
+
+
+class App extends React.Component {
+
+}
+ReactDOM.render(<Match/>, document.getElementById("root"));
+
+
